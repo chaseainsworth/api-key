ffc340b270f2ad9592761dd7710a55a4c1d1e31f
Chase Ainsworth <chase.ainsworth@codeimmersives.com>
2020-09-30 08:56:04 -0400

    initial

---
+++ key.js
@@ -0,0 +1,3 @@
+let apiKey = 'apiKey=c7ae4a9da35a40db80fe6927b16388ff';
+
+module.exports = apiKey;
--- main.js
+++ main.js
@@ -6,22 +6,17 @@
 // Use any tools we've practiced such as promises, destructuring, etc.
 // Once everything works, upload the file and submit.
 
+const fetch = require('node-fetch');
+const apiKey = require('./key.js');
 
-// // Weather API- https://openweathermap.org/api
-// // Weather/Air Quality API-  https://www.airvisual.com/dashboard/api
-// // Recipe API- https://developer.edamam.com/ (uses two key code)
-// // Superheroes API- https://superheroapi.com/index.html
-// // Movies API- https://www.omdbapi.com/
-// // Sports API - https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=Arsenal
-// // News API - https://newsapi.org/
-// // Harvard Art Museum API - https://www.harvardartmuseums.org/collections/api
+var url = 'http://newsapi.org/v2/everything?' +
+        'q=space&' +
+        'from=2020-09-30&' +
+        'sortBy=popularity&' +
+        apiKey;
+
+fetch(url)
+    .then(res => res.json())
+    .then(json => console.log(json.author));
 
 
-// If you want to make your project a little more robust and dynamic you could use tools like the ones below:
-// Read the docs to see how it works. Not hard.
-
-
-// Readline: https://nodejs.org/api/readline.html
-// OR
-// Inquirer: https://www.npmjs.com/package/inquirer
-// Read the docs to find out how to use. Pretty intuitive.
