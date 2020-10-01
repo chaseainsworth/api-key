80b57ad041afa944e29263a20a4519bdd2105db1
Chase Ainsworth <chase.ainsworth@codeimmersives.com>
2020-10-01 08:05:59 -0400

    done

--- main.js
+++ main.js
@@ -10,13 +10,26 @@ const fetch = require('node-fetch');
 const apiKey = require('./key.js');
 
 var url = 'http://newsapi.org/v2/everything?' +
-        'q=space&' +
+        'q=cats&' +
         'from=2020-09-30&' +
         'sortBy=popularity&' +
         apiKey;
 
 fetch(url)
-    .then(res => res.json())
-    .then(json => console.log(json.author));
+        .then(res => res.json())
+        .then(json => {
+                const {articles} = json;
+                console.log(`There have been ${articles.length} new articles on the topic of "Cats" in the last 24 hours.`)
+                const {articles: [{author: a, title: t, url}]} = json;
+                let articlesObj = {};
+                json.articles.map((article, index) => articlesObj[index + 1] = `${article.title}. \n Author: ${article.author} \n ${url}`);
+                return articlesObj;
+        })
+        .then(title => {
+                console.log(title);
+        })
+        .catch(err => {
+                console.log(err);
+        })
 
 
